8f9ceb5a67731cbcb8067a571470af3abf911cbe
kroppheather <kroppheather@gmail.com>
2023-11-21 10:58:28 -0500

    training but model seems to be over fit. Rerunning

---
+++ prediction.R
@@ -0,0 +1,32 @@
+library(terra)
+library(dplyr)
+
+
+# Image merge ---------
+
+dirP <- "/media/hkropp/research/milkweed/predictions/run01"
+
+filesL <- list.files(dirP)
+
+Nimg <- length(filesL)
+
+
+milkImg <- list()
+
+for(i in 1:Nimg){
+  milkImg[[i]] <- rast(paste0(dirP,"/",filesL[i]))
+  
+  
+}
+
+milkAll <- do.call(merge, milkImg)
+plot(milkAll)
+
+
+milkMap1 <- ifel(milkAll >= 0.1, 1, 0) 
+
+milkMap2 <- ifel(milkAll >= 0.6, 1, 0) 
+
+writeRaster(milkAll, "/media/hkropp/research/milkweed/predictions/maps/milk_prob.tif", filetype="GTiff" )
+writeRaster(milkMap1, "/media/hkropp/research/milkweed/predictions/maps/milk_T1.tif", filetype="GTiff" )
+writeRaster(milkMap2, "/media/hkropp/research/milkweed/predictions/maps/milk_T2.tif", filetype="GTiff" )
