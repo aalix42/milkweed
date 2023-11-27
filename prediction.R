b078f447632ac3b7fca22b9bb7660b8cf47b0c65
kroppheather <kroppheather@gmail.com>
2023-11-27 10:33:19 -0500

    finished training

--- prediction.R
+++ prediction.R
@@ -4,7 +4,7 @@ library(dplyr)
 
 # Image merge ---------
 
-dirP <- "/media/hkropp/research/milkweed/predictions/run01"
+dirP <- "/media/hkropp/research/milkweed/predictions/run02"
 
 filesL <- list.files(dirP)
 
@@ -27,6 +27,6 @@ milkMap1 <- ifel(milkAll >= 0.1, 1, 0)
 
 milkMap2 <- ifel(milkAll >= 0.6, 1, 0) 
 
-writeRaster(milkAll, "/media/hkropp/research/milkweed/predictions/maps/milk_prob.tif", filetype="GTiff" )
-writeRaster(milkMap1, "/media/hkropp/research/milkweed/predictions/maps/milk_T1.tif", filetype="GTiff" )
-writeRaster(milkMap2, "/media/hkropp/research/milkweed/predictions/maps/milk_T2.tif", filetype="GTiff" )
+writeRaster(milkAll, "/media/hkropp/research/milkweed/predictions/maps/run02/milk_prob.tif", filetype="GTiff" )
+writeRaster(milkMap1, "/media/hkropp/research/milkweed/predictions/maps/run02/milk_T1.tif", filetype="GTiff" )
+writeRaster(milkMap2, "/media/hkropp/research/milkweed/predictions/maps/run02/milk_T2.tif", filetype="GTiff" )
